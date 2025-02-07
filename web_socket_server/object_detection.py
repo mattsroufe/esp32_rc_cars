9f7751e586b374a1fbeabfbac3aed0bd9a3ee99b
Matt Sroufe <mattsroufe@gmail.com>
2025-02-08 11:44:44 +1300

    remove tracking

--- web_socket_server/object_detection.py
+++ web_socket_server/object_detection.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import cv2
 import numpy as np
+import time
 import torch
 from ultralytics import YOLO
 
@@ -17,16 +18,6 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-track_history = defaultdict(lambda: [])
-# get the video properties
-fps = int(cap.get(cv2.CAP_PROP_FPS))
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# define the codec and create VideoWriter object
-output_path = "output_tracked_video.mp4"  # Output video file path
-out = cv2.VideoWriter(
-    output_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (frame_width, frame_height)
-)
 # loop through the video frames
 while cap.isOpened():
     ret, frame = cap.read()
@@ -36,29 +27,7 @@ while cap.isOpened():
 
     results = model.track(frame, persist=True)
     boxes = results[0].boxes.xywh.cpu()
-    track_ids = (
-        results[0].boxes.id.int().cpu().tolist()
-        if results[0].boxes.id is not None
-        else None
-    )
     annotated_frame = results[0].plot()
-    # plot the tracks
-    if track_ids:
-        for box, track_id in zip(boxes, track_ids):
-            x, y, w, h = box
-            track = track_history[track_id]
-            track.append((float(x), float(y)))  # x, y center point
-            if len(track) > 30:  # retain 30 tracks for 30 frames
-                track.pop(0)
-            # draw the tracking lines
-            points = np.array(track).astype(np.int32).reshape((-1, 1, 2))
-            cv2.polylines(
-                annotated_frame,
-                [points],
-                isClosed=False,
-                color=(230, 230, 230),
-                thickness=2,
-            )
 
     # Show the output
     cv2.imshow("YOLOv8n Webcam Detection (640x480)", annotated_frame)
